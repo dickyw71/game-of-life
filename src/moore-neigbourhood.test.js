2889753aa66e447febbe7fd411f2f99c2ba20436
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-05-19 22:45:04 +0100

    Moving towards a solution.

--- src/moore-neigbourhood.test.js
+++ src/moore-neigbourhood.test.js
@@ -32,11 +32,11 @@ class Cell {
     }  
 }
 
-var a = new Array(50);
+var cellGrid = new Array(50);
 for (let i = 0; i < 50; i++) {
-    a[i] = new Array(50);
+    cellGrid[i] = new Array(50);
     for (let j = 0; j < 50; j++) {
-        a[i][j] = new Cell(i, j, getRandomBoolean());
+        cellGrid[i][j] = new Cell(i, j, getRandomBoolean());
     }
 }
 
@@ -46,10 +46,8 @@ function findLiveCells(cellGrid) {
     return flattenedGrid.filter(function(cell) {
         return cell.isAlive;
     }) 
-   // return cellGrid; 
 }
 
-console.log(findLiveCells(a))
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -60,8 +58,6 @@ function getRandomIntInclusive(min, max) {
 function getRandomBoolean() {
     return getRandomIntInclusive(0, 1);
 }
-
-console.log(new Cell(4, 4, getRandomBoolean()));
 
 // flatten is called recursively
 // for each element that is an array 
@@ -81,3 +77,95 @@ function steamroller(arr) {
   // flatten arrays into one
   return arr.reduce(flatten, []);     
 }
+
+let blinkerGrid = [ new Cell(0,0,false), new Cell(1,0,false), new Cell(2,0,false), new Cell(3,0,false), new Cell(4,0,false),
+                    new Cell(0,1,false), new Cell(1,1,false), new Cell(2,1,true), new Cell(3,1,false), new Cell(4,1,false), 
+                    new Cell(0,2,false), new Cell(1,2,false), new Cell(2,2,true), new Cell(3,2,false), new Cell(4,2,false), 
+                    new Cell(0,3,false), new Cell(1,3,false), new Cell(2,3,true), new Cell(3,3,false), new Cell(4,3,false), 
+                    new Cell(0,4,false), new Cell(1,4,false), new Cell(2,4,false), new Cell(3,4,false), new Cell(4,4,false)];
+
+let blinkerGrid2D = [[new Cell(0,0,false), new Cell(1,0,false), new Cell(2,0,false), new Cell(3,0,false), new Cell(4,0,false)],
+                    [new Cell(0,1,false), new Cell(1,1,false), new Cell(2,1,true), new Cell(3,1,false), new Cell(4,1,false)],
+                    [new Cell(0,2,false), new Cell(1,2,false), new Cell(2,2,true), new Cell(3,2,false), new Cell(4,2,false)],
+                    [new Cell(0,3,false), new Cell(1,3,false), new Cell(2,3,true), new Cell(3,3,false), new Cell(4,3,false)],
+                    [new Cell(0,4,false), new Cell(1,4,false), new Cell(2,4,false), new Cell(3,4,false), new Cell(4,4,false)]];
+
+
+let liveNeighbourhoods = [];
+
+for(let y=0; y<blinkerGrid2D.length;y++) {
+    for(let x=0; x<blinkerGrid2D.length; x++) {
+        if(blinkerGrid2D[y][x].isAlive == true) {
+            let neigbourhood = [];
+            neigbourhood.push(blinkerGrid2D[y][x]);
+            neigbourhood.push(blinkerGrid2D[y][x-1]);
+            neigbourhood.push(blinkerGrid2D[y-1][x-1]);
+            neigbourhood.push(blinkerGrid2D[y-1][x]);
+            neigbourhood.push(blinkerGrid2D[y-1][x+1]);
+            neigbourhood.push(blinkerGrid2D[y][x+1]);
+            neigbourhood.push(blinkerGrid2D[y+1][x+1]);
+            neigbourhood.push(blinkerGrid2D[y+1][x]);
+            neigbourhood.push(blinkerGrid2D[y+1][x-1]);
+            
+            liveNeighbourhoods.push(neigbourhood);
+        }
+    }
+} 
+
+console.log(liveNeighbourhoods);
+
+function sumLiveCellsInNeigbourhood(grid) {
+    let sum = 0;
+    grid.forEach((cell) => {
+        if(cell.isAlive) {
+            sum++;
+        }
+    })
+    return sum;
+}
+
+function nextGeneration(cell, index, arr) {
+ 
+    
+    // if cell is in a live neighbourhood calculate it's next state
+    // else cell is dead
+    let cellInLiveNeigbourhood = false;
+    for(let neigbourhood of liveNeighbourhoods) {
+        if (neigbourhood.some( el => el.x === cell.x && el.y === cell.y)) {
+            // cell found
+            cellInLiveNeigbourhood = true;
+            console.log(cell);
+            break; // exit for...of
+        }
+    }
+    
+    let sum = sumLiveCellsInNeigbourhood(arr);
+    let _cell = new Cell(cell.x, cell.y, cell.isAlive);
+    switch(sum) {
+        case 3: 
+        // Life
+        _cell.isAlive = true;
+        break;
+        case 4:
+        // centre cell stays the same
+        break;
+        default:
+        // Death
+        _cell.isAlive = false;
+        break;
+    }
+    return _cell;
+}
+
+console.log(blinkerGrid);
+
+let blinkerGrid2 = blinkerGrid.map(nextGeneration);
+console.log(blinkerGrid);
+console.log(blinkerGrid2);
+
+let blinkerGrid3 = blinkerGrid2.map(nextGeneration);
+console.log(blinkerGrid3);
+
+
+
+
--- test-list.md
+++ test-list.md
@@ -1,2 +1,59 @@
 ## Tests
-- The moore neigbourhood module should calculate which cells are alive
+- The moore neigbourhood module should calculate which cells are alive in the next generation based on the current generation
+
+Algorithm 
+
+for each live cell
+    sum the number of adjacent live cells within the moore-neighbourhood
+    if sum is 2 or 3  
+        cell remains alive
+    else
+        cell dies
+
+http://rosettacode.org/wiki/Conway%27s_Game_of_Life
+
+A cell   C   is represented by a   1   when alive,   or   0   when dead,   in an   m-by-m   (or m×m)   square array of cells.
+
+We calculate   N   - the sum of live cells in C's   eight-location neighbourhood,   then cell   C   is alive or dead in the next generation based on the following table:
+
+   C   N                 new C
+   1   0,1             ->  0  # Lonely
+   1   4,5,6,7,8       ->  0  # Overcrowded
+   1   2,3             ->  1  # Lives
+   0   3               ->  1  # It takes three to give birth!
+   0   0,1,2,4,5,6,7,8 ->  0  # Barren
+
+   C is initial condition - > Pass Cell to Reducer as initial value, then reducer calculates the value of N
+
+To avoid decisions and branches in the counting loop, the rules can be rearranged from an egocentric approach of the inner field regarding its neighbours to a scientific observer's viewpoint: if the sum of all nine fields is 3, the inner field state for the next generation will be life (no matter of its previous contents); if the all-field sum is 4, the inner field retains its current state and every other sum sets the inner field to death.
+
+
+Algorithm
+Input: A square tessellation, T, containing a connected component P of black cells.
+Output: A sequence B (b1, b2, ..., bk) of boundary pixels i.e. the contour.
+Define M(a) to be the Moore neighborhood of pixel a.
+Let p denote the current boundary pixel.
+Let c denote the current pixel under consideration i.e. c is in M(p).
+Let b denote the backtrack of c (i.e. neighbor pixel of p that was previously tested)
+ 
+Begin
+  Set B to be empty.
+  From bottom to top and left to right scan the cells of T until a black pixel, s, of P is found.
+  Insert s in B.
+  Set the current boundary point p to s i.e. p=s
+  Let b = the pixel from which s was entered during the image scan.
+  Set c to be the next clockwise pixel (from b) in M(p).
+  While c not equal to s do
+    If c is black
+      insert c in B
+      Let b = p
+      Let p = c
+      (backtrack: move the current pixel c to the pixel from which p was entered)
+      Let c = next clockwise pixel (from b) in M(p).
+    else
+      (advance the current pixel c to the next clockwise pixel in M(p) and update backtrack)
+      Let b = c
+      Let c = next clockwise pixel (from b) in M(p).
+    end If
+  end While
+End
