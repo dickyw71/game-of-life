8bb0c8bc93b3f789605dc46ecaefdbbd7acdb5c0
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-06-04 21:56:12 +0100

    A class module that represents a cell in the game.

---
+++ src/cell.js
@@ -0,0 +1,31 @@
+class Cell {
+    constructor(x, y, isAlive) {
+        this.x = x;
+        this.y = y;
+        this.isAlive = isAlive;
+    }  
+
+    /**
+     * Determine if a cell will live or die based on the sum of the live cells
+     * in the neighourhood and whether it is currently alive or dead
+     * @param {Number} sumLiveCells 
+     * @param {Boolean} isAlive 
+     */
+    static prognosis(sumLiveCells, isAlive) {
+        let life = isAlive;
+        switch(sumLiveCells) {
+            case 3: 
+            life = true;       // Life!
+            break;
+            case 4:            // Cell condition remains the same
+            break;
+            default:
+            // Death
+            life = false;
+            break;
+        } 
+        return life;
+    }
+}
+
+export default Cell;
