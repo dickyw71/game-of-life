f03c68cf6e096d2605569c5b26966c7fc2aa2333
Richard Wheatley <dickyw71@users.noreply.github.com>
2017-06-06 21:36:24 +0100

    Added constructor and methods for Game Controls.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,41 @@ import './App.css';
 import { GameControls, GameGenerationCounter, GameBoard } from './game-of-life.js';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { generation: 1 }
+    let timerId = 0;
+
+    this.newGeneration = this.newGeneration.bind(this);
+    this.startGame = this.startGame.bind(this);
+    this.stopGame = this.stopGame.bind(this);
+    this.clearGame = this.clearGame.bind(this);
+
+  }
+
+  newGeneration() {
+    this.setState((prevState) => {
+      return {generation: prevState.generation + 1 };
+    })
+  }
+
+
+  startGame() {
+      this.timerId = setInterval(this.newGeneration, 1000);    
+  }
+
+  stopGame() {
+    clearInterval(this.timerId);
+  }
+
+  clearGame() {
+    // to-do
+  }
+
+ componentDidMount() {
+      this.startGame();
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,9 +46,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Conway's Game of Life</h2>
         </div>
-        <GameControls />
-        <GameGenerationCounter />
-        <GameBoard />
+        <GameControls startGame={this.startGame} stopGame={this.stopGame} clearGame={this.clearGame}/>
+        <GameGenerationCounter genCount={this.state.generation}/>
+        <GameBoard genCount={this.state.generation}/>
       </div>
     );
   }
